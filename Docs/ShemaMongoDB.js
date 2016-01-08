2a02ce231efc8611417304f13811f6abc95eff24
Vejron <bjorn.yttergren@gmail.com>
2016-01-08 09:08:07 +0100

    tinkering with DB schema

---
+++ Docs/ShemaMongoDB.js
@@ -0,0 +1,14 @@
+var Shema = {
+    _id: 34534534533453,
+    date: '2016-11-07 / 23:11:01',
+    table_id: 0, //0-15
+    session_name: 'festivalen i rio',
+    maxCapacity: 1000,
+    granularity: '100ms', // 0.1 * 1000
+    transforms: [{
+        time: 'date_t',
+        rvec: [0, 0, 0],
+        tvec: [0, 0, 0],
+        grab: false
+    }]
+}
